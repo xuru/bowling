ee11c5b986634e17d2fa41cc9b250f043f20711a
Eric Plaster <eric.plaster@myxer.com>
2014-07-05 04:32:08 -0500

    Cleaned up the demo script a bit

--- demo.py
+++ demo.py
@@ -2,19 +2,39 @@ from requests import post, put, get, delete
 from random import randrange
 
 
-def new_game():
-    # delete any games currently in the system (not really neccessary)
-    games = get('http://localhost:5000/v1/games').json()
-    for game in games:
-        delete('http://localhost:5000/v1/games/{}'.format(game['id']))
+HOST = 'http://localhost:5000'
+VERSION = 'v1'
 
-    return post('http://localhost:5000/v1/games', data={'players': ['Joe Johnson', 'James Anderson']}).json()
+
+def abs_url(part):
+    return '/'.join([HOST, VERSION, part])
+
+
+def new_game():
+    """
+    Create a new game using only requests
+    """
+    # delete any games currently in the system (not really neccessary)
+    games = get(abs_url('games')).json()
+    for game in games:
+        delete(abs_url('games/{}'.format(game['id'])))
+
+    return post(abs_url('games'), data={'players': ['Joe Johnson', 'James Anderson']}).json()
 
 
 def send_roll(game, player, pins):
-    return put("http://localhost:5000/v1/games/{}/roll".format(game['id']), data={'player_id': player['id'], 'pins': pins}).json()
+    """
+    Send a new roll to the API server
+    """
+    return put(
+        abs_url("games/{}/roll".format(game['id'])),
+        data={'player_id': player['id'], 'pins': pins}).json()
 
-def do_rolls(game, player, frame, force_pins=None):
+
+def turn(game, player, frame, force_pins=None):
+    """
+    Simulate a players turn
+    """
     rolls = []
     remainder = 10  # so we get a range of 0 - 10
     for turn in range(2):
@@ -35,24 +55,32 @@ def do_rolls(game, player, frame, force_pins=None):
         send_roll(game, player, pins)
 
 
-game = new_game()
-for player in game['players']:
-    for frame in range(10):
-        do_rolls(game, player, frame + 1)
+def print_scores(game):
+    """
+    Print out the scores for all the players in the game
+    """
+    print "score..."
+    game = get(abs_url('games/{}'.format(game['id']))).json()
+    players = game['players']
 
-print "score..."
-game = get('http://localhost:5000/v1/games/{}'.format(game['id'])).json()
-players = game['players']
-
-fmt = "%-30s "
-for player in players:
-    fmt += "%-20s "
-names = [p['name'] for p in players]
-print fmt % tuple([' '] + names)
-
-for frame in range(10):
-    scores = []
+    fmt = "%-30s "
     for player in players:
-        scores.append(player['frames'][frame]['score'])
+        fmt += "%-20s "
+    names = [p['name'] for p in players]
+    print fmt % tuple([' '] + names)
 
-    print fmt % tuple(['Frame %d' % (frame + 1)] + scores)
+    for frame in range(10):
+        scores = []
+        for player in players:
+            scores.append(player['frames'][frame]['score'])
+
+        print fmt % tuple(['Frame %d' % (frame + 1)] + scores)
+
+
+if __name__ == '__main__':
+    game = new_game()
+    for player in game['players']:
+        for frame in range(10):
+            turn(game, player, frame + 1)
+
+    print_scores(game)
